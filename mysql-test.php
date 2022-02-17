60038d726a16b4e739502f35167a169c3268a904
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-02-17 14:58:29 +0100

    Exercises

--- mysql-test.php
+++ mysql-test.php
@@ -4,16 +4,33 @@ require_once "DB.php";
 require_once "DB_functions.php";
 require_once "Country.php";
 
+
+$get_cities = function($page_nr)
+{
+    $offset = ($page_nr-1) * 20;
+
+    $query = "
+    SELECT `cities`.*
+    FROM `cities`
+    ORDER BY `cities`.`id` ASC
+    LIMIT  {$offset}, 20
+    ";
+
+    return $query;
+};
+
+
 $success = connect("localhost", "world", "root", "");
 
 // var_dump($success);
 
-$query = "
-    SELECT `countries`.*
-    FROM `countries`
-    WHERE `population` > ?
-";
+// $query = "
+//     SELECT `cities`.*
+//     FROM `cities`
+//     ORDER BY `cities`.`population` DESC
+//     LIMIT 100
+// ";
 
-$result = select($query, [200000000], "Country");
+$result = select($get_cities(5));
 
 var_dump($result);
