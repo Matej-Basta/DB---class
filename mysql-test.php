546cd0a3a722250f2cf215d2c70694d44ffa36fc
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-02-17 13:24:02 +0100

    Country class created

--- mysql-test.php
+++ mysql-test.php
@@ -2,7 +2,19 @@
 
 require_once "DB.php";
 require_once "DB_functions.php";
+require_once "Country.php";
 
 $success = connect("localhost", "world", "root", "");
 
-var_dump($success);
+// var_dump($success);
+
+$query = "
+    SELECT `cities`.*
+    FROM `cities`
+    WHERE `population` > 2000000
+    OR `country_id` = 2
+";
+
+$result = select($query);
+
+var_dump($result);
